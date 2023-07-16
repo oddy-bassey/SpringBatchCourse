6e5057f2c42790677ec79a839aa6a37cc47fd377
oddy-bassey <oddybassey@gmail.com>
2023-07-16 20:57:01 +0100

    creating item writer

---
+++ spring-batch-section5/src/main/java/com/vaultcode/springbatchsection5/writer/DataItemWriter.java
@@ -0,0 +1,16 @@
+package com.vaultcode.springbatchsection5.writer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DataItemWriter implements ItemWriter<Long> {
+    @Override
+    public void write(Chunk<? extends Long> chunk) throws Exception {
+        log.info("*** Inside item writer!");
+        chunk.getItems().forEach(data -> log.info(String.valueOf(data)));
+    }
+}
