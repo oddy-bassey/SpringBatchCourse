b82698891e00dcf571e4bf6c3666de604d68b7c4
oddy-bassey <oddybassey@gmail.com>
2023-07-16 20:41:33 +0100

    creating an item processor

---
+++ spring-batch-section5/src/main/java/com/vaultcode/springbatchsection5/processor/DataItemProcessor.java
@@ -0,0 +1,16 @@
+package com.vaultcode.springbatchsection5.processor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DataItemProcessor implements ItemProcessor<Integer, Long> {
+
+    @Override
+    public Long process(Integer item) throws Exception {
+        log.info("*** Inside the item processor!");
+        return (long) (item + 20);
+    }
+}
